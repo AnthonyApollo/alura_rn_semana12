c1c260f11a942394795194daebb63160adcd828e
AnthonyApollo <anthonyapollo@outlook.com>
2023-06-06 11:31:59 -0300

    feat: Add icons to tabBar.

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,14 +1,32 @@
-import React from "react"
-import { NavigationContainer } from "@react-navigation/native"
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "../screens/Home"
+
+import Home from "../screens/Home";
 import Habits from "../screens/Habits";
+
+import ListIcon from "./assets/list.svg";
+import HeatLine from "./assets/heartline.svg"
 
 export default function AppRoutes() {
     const Tab = createBottomTabNavigator();
 
     return <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color }) => {
+                let Icon = ListIcon
+
+                if (route.name === 'Habits') {
+                    Icon = HeatLine
+                }
+
+                return <Icon color={color} />
+            },
+            tabBarActiveTintColor: 'black',
+            tabBarInactiveTintColor: '#A7A7A7',
+        })}>
             <Tab.Screen name="Home" component={Home} />
             <Tab.Screen name="Habits" component={Habits} />
         </Tab.Navigator>
--- src/screens/Habits/components/HabitsListHeader.js
+++ src/screens/Habits/components/HabitsListHeader.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 
-export default function HabitsListHeader({title, image}) {
+export default function HabitsListHeader({ title, image }) {
     return <View style={styles.content}>
-        <Image source={image}/>
+        <Image source={image} />
         <Text style={styles.title}>{title}</Text>
     </View>
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     content: {
         flexDirection: 'row',
         alignItems: 'center',
